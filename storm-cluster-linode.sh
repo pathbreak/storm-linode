14664e4f1efd8a7bf62b11c80a0219b530c82b3c
Karthik Shiraly <pathbreak.github@gmail.com>
2015-12-30 05:39:06 +0530

    Describe cluster

--- storm-cluster-linode.sh
+++ storm-cluster-linode.sh
@@ -2199,6 +2199,100 @@ update_client_user_whitelist() {
 
 
 # $1 : The cluster conf file
+describe_cluster() {
+	if [ ! -f $1 ]; then
+		printf "Configuration file $1 not found.\nUsage: storm-cluster-linode.sh describe CLUSTER-CONF-FILE\n"
+		return 1
+	fi
+	
+	init_conf $1
+	if [ $? -eq 1 ]; then
+		return 1
+	fi
+
+	local stfile="$(status_file)"
+	# If the cluster does not exist, abort this addition.
+	if [ ! -f "$stfile" ]; then
+		echo "Cluster is not created. Only existing clusters can be described."
+		return 1
+	fi
+	
+	local cluster_status=$(get_cluster_status)
+	printf "\nStatus: $cluster_status\n\n"
+	
+	local nodes=$(get_section $stfile "nodes")
+	local hostnames=$(get_section $stfile "hostnames")
+	local ipaddrs=$(get_section $stfile "ipaddresses")
+
+	local nimbus_linode_id=$(echo "$nodes"|grep 'nimbus'|cut -d ':' -f1)
+	local nimbus_ipline=$(echo "$ipaddrs"|grep "$nimbus_linode_id")
+	local nimbus_iparr=($nimbus_ipline)
+	local nimbus_private_ip=${nimbus_iparr[1]}
+	local nimbus_public_ip=${nimbus_iparr[2]}
+	local nimbus_hostsline=$(echo "$hostnames"|grep "$nimbus_linode_id")
+	local nimbus_hostsarr=($nimbus_hostsline)
+	local nimbus_private_host=${nimbus_hostsarr[1]}
+	local nimbus_public_host=${nimbus_hostsarr[2]}
+		
+	cat <<-ENDSTANZA
+		Nimbus:
+		  Linode ID:		$nimbus_linode_id
+		  Private IP:		$nimbus_private_ip
+		  Private hostname:	$nimbus_private_host
+		  Public IP:		$nimbus_public_ip
+		  Public hostname:	$nimbus_public_host
+		  
+	ENDSTANZA
+	
+	local client_linode_id=$(echo "$nodes"|grep 'client'|cut -d ':' -f1)
+	local client_ipline=$(echo "$ipaddrs"|grep "$client_linode_id")
+	local client_iparr=($client_ipline)
+	local client_private_ip=${client_iparr[1]}
+	local client_public_ip=${client_iparr[2]}
+	local client_hostsline=$(echo "$hostnames"|grep "$client_linode_id")
+	local client_hostsarr=($client_hostsline)
+	local client_private_host=${client_hostsarr[1]}
+	local client_public_host=${client_hostsarr[2]}
+		
+	cat <<-ENDSTANZA
+		Client:
+		  Linode ID:		$client_linode_id
+		  Private IP:		$client_private_ip
+		  Private hostname:	$client_private_host
+		  Public IP:		$client_public_ip
+		  Public hostname:	$client_public_host
+		  
+	ENDSTANZA
+
+	echo 'Supervisors:'
+	
+	while read node;
+	do
+		local sup_linode_id=$(echo "$node"|cut -d ':' -f1)
+		local sup_ipline=$(echo "$ipaddrs"|grep "$sup_linode_id")
+		local sup_iparr=($sup_ipline)
+		local sup_private_ip=${sup_iparr[1]}
+		local sup_public_ip=${sup_iparr[2]}
+		local sup_hostsline=$(echo "$hostnames"|grep "$sup_linode_id")
+		local sup_hostsarr=($sup_hostsline)
+		local sup_private_host=${sup_hostsarr[1]}
+		local sup_public_host=${sup_hostsarr[2]}
+
+		cat <<-ENDSTANZA
+			  Linode ID:		$sup_linode_id
+			  Private IP:		$sup_private_ip
+			  Private hostname:	$sup_private_host
+			  Public IP:		$sup_public_ip
+			  Public hostname:	$sup_public_host
+			  
+		ENDSTANZA
+	
+	done <<< "$(echo "$nodes" | grep 'supervisor')"
+}
+
+
+
+# $1 : The cluster conf file
 # $2... : Command to be run on all nodes of cluster.
 run_cmd() {
 	if [ ! -f $1 ]; then
@@ -2816,6 +2910,10 @@ case $1 in
 	
 	update-user-whitelist)
 	update_client_user_whitelist $2
+	;;
+	
+	describe)
+	describe_cluster $2
 	;;
 	
 	run)
